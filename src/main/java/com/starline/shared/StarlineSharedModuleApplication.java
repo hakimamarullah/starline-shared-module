09389dc751cfac56d7e15d8559908612f2ff37a3
hakimamarullah <hakimamarullah@gmail.com>
2024-09-18 14:45:14 +0700

    Initial commit

---
+++ src/main/java/com/starline/shared/StarlineSharedModuleApplication.java
@@ -0,0 +1,13 @@
+package com.starline.shared;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlineSharedModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarlineSharedModuleApplication.class, args);
+	}
+
+}
